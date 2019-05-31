b7ed21700a801dd4b624225825ca303c9bf10eb6
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2019-05-31 22:24:27 +0200

    refactor: strip to basics, reorder metadata

--- projects/from-store-feature/src/app/app.module.ts
+++ projects/from-store-feature/src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
 })
-export class AppModule { }
+export class AppModule {}
