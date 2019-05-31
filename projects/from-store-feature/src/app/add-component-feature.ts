5a311aa9600b7030cbebb3fc01159e6848c65e0a
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2019-06-01 00:52:24 +0200

    feat: apply features manually

--- projects/from-store-feature/src/app/add-component-feature.ts
+++ projects/from-store-feature/src/app/add-component-feature.ts
@@ -3,6 +3,8 @@ import { ɵComponentDef as ComponentDef } from '@angular/core';
 export type Feature<T> = (componentDef: ComponentDef<T>) => void;
 
 export function addComponentFeature<T>(feature: Feature<T>, componentType) {
+  feature(componentType['ngComponentDef']);
+
   componentType['ngComponentDef'].features =
     componentType['ngComponentDef'].features || [];
   componentType['ngComponentDef'].features = [
