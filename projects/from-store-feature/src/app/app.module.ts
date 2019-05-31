9da1782df675ed8ab14e252025a049768ac27c18
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2019-05-31 22:39:11 +0200

    feat: add NgRx Store

--- projects/from-store-feature/src/app/app.module.ts
+++ projects/from-store-feature/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers })],
 })
 export class AppModule {}
