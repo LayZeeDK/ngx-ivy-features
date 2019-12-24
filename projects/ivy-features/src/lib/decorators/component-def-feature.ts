65638cf567187bcdd7977a27e23c9af459ea30b2
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2019-12-24 01:51:44 +0100

    style: format

--- projects/ivy-features/src/lib/decorators/component-def-feature.ts
+++ projects/ivy-features/src/lib/decorators/component-def-feature.ts
@@ -3,12 +3,12 @@ import { ɵComponentDef as ComponentDef } from '@angular/core';
 export interface ComponentDefFeature {
   <T>(componentDef: ComponentDef<T>): void;
   /**
-   * Marks a feature as something that {@link InheritDefinitionFeature} will execute
-   * during inheritance.
+   * Marks a feature as something that {@link InheritDefinitionFeature} will
+   * execute during inheritance.
    *
-   * NOTE: DO NOT SET IN ROOT OF MODULE! Doing so will result in tree-shakers/bundlers
-   * identifying the change as a side effect, and the feature will be included in
-   * every bundle.
+   * NOTE: DO NOT SET IN ROOT OF MODULE! Doing so will result in
+   * tree-shakers/bundlers identifying the change as a side effect, and the
+   * feature will be included in every bundle.
    */
   ngInherit?: true;
 }
